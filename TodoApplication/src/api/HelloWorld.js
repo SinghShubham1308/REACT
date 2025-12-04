da4fd1d681099f0f14062c8e7faf856973ad504b
SinghShubham1308 <shubhusingh1308@gmail.com>
2025-12-04 23:36:32 +0530

    Delete TodoApplication

--- TodoApplication/src/api/HelloWorld.js
+++
@@ -1,8 +0,0 @@
-import { apiClient } from "./apiClient";
-
-export const retreiveHello = () => apiClientlient.get("/hello-world");
-
-export const retreiveHelloWorldBean = () => apiClient.get("/hello-world-bean");
-
-export const retreiveHelloWorldPathVariable = (username) =>
-  apiClient.get(`/hello-world/path-variable/${username}`);
--- TodoApplication/src/api/apiClient.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const apiClient = axios.create({
-  baseURL: "http://localhost:9090",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  withCredentials: true,
-});
